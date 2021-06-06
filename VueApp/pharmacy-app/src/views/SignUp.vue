545b2ec1265cb041b808a3933f784cca1c0896c5
Damian <daamian299@gmail.com>
2021-06-06 20:03:39 +0200

    Initial Sign Up Validation

--- VueApp/pharmacy-app/src/views/SignUp.vue
+++ VueApp/pharmacy-app/src/views/SignUp.vue
@@ -1,31 +1,31 @@
 <template>
     <b-container class="mt-5">
-        <b-form >
+        <b-form @submit.stop.prevent="signUp">
             <b-row class="justify-content-md-center">
                 <b-col class="col-lg-3">
                     <b-form-group
                     label="First name"
                     label-for="firstName-input"
-                    invalid-feedback="First name is required">
+                    invalid-feedback="Enter at least 3 characters.">
                         <b-form-input
                             id="firstName-input"
                             type="text"
                             v-model="userCredentials.firstName"
-                            required
-                            @keydown.native.enter="signUp"/>
+                            :state="firstNameState"
+                            required/>
                     </b-form-group>
                 </b-col>
                 <b-col class="col-lg-3">
                     <b-form-group
                     label="Last name"
                     label-for="lastName-input"
-                    invalid-feedback="Last name is required">
+                    invalid-feedback="Enter at least 3 characters.">
                         <b-form-input
                             id="lastName-input"
                             type="text"
                             v-model="userCredentials.lastName"
-                            required
-                            @keydown.native.enter="signUp"/>
+                            :state="lastNameState"
+                            required/>
                     </b-form-group>
                 </b-col>
             </b-row>  
@@ -34,25 +34,25 @@
                     <b-form-group
                     label="Email"
                     label-for="email-input"
-                    invalid-feedback="Email is required">
+                    invalid-feedback="Invalid email address.">
                         <b-form-input
                             id="email-input"
                             type="email"
                             v-model="userCredentials.email"
-                            required
-                            @keydown.native.enter="signUp"/>
+                            :state="emailState"
+                            required/>
                     </b-form-group>
                 </b-col>
                 <b-col class="col-lg-3">
                     <b-form-group
                     label="Phone number"
                     label-for="phone-input"
-                    invalid-feedback="Phone is required">
+                    invalid-feedback="Invalid phone number.">
                         <b-form-input
                             id="phone-input"
                             type="tel"
                             v-model="userCredentials.phone"
-                            @keydown.native.enter="signUp"/>
+                            :state="phoneState"/>
                     </b-form-group>
                 </b-col>
             </b-row>
@@ -61,12 +61,12 @@
                     <b-form-group
                         label="Date of birth"
                         label-for="age-input"
-                        invalid-feedback="Age is required">
+                        invalid-feedback="Date of birth is required.">
                         <b-form-input
                             id="age-input"
                             type="date"
-                            v-model="userCredentials.age"
-                            @keydown.native.enter="signUp"/>
+                            v-model="userCredentials.dateOfBirth"
+                            :state="dateOfBirthState"/>
                     </b-form-group>
                 </b-col>
                 <b-col class="col-lg-3">
@@ -82,55 +82,56 @@
                     <b-form-group
                     label="Password"
                     label-for="password-input"
-                    invalid-feedback="Password is required">
+                    invalid-feedback="Password must have at least 4 characters.">
                         <b-form-input
                             id="password-input"
                             type="password"
                             v-model="userCredentials.password"
-                            required
-                            @keydown.native.enter="signUp"/>
+                            :state="passwordState"
+                            required/>
                     </b-form-group>
                 </b-col>
                 <b-col class="col-lg-3">
                     <b-form-group
                     label="Password confirm"
                     label-for="passwordConf-input"
-                    invalid-feedback="Password is incorrect">
+                    invalid-feedback="Passwords do not match.">
                         <b-form-input
                             id="passwordConf-input"
                             type="password"
-                            required
-                            @keydown.native.enter="signUp"/>
+                            v-model="userCredentials.confirmPassword"
+                            :state="passwordConfirmState"
+                            required/>
                     </b-form-group>
                 </b-col>
             </b-row>
             <b-row class="justify-content-md-center mt-3">
                 <b-col class="col-lg-3 text-center">
-                    <b-button variant="primary" size="lg" @click="signUp">Sign up</b-button>
-                </b-col>
-            </b-row>
-            <b-row class="justify-content-md-center mt-3">
-                <b-col class="col-lg-6">
-                    <hr class="or">
-                </b-col>
-            </b-row>
-            <b-row class="justify-content-md-center mt-3 mb-3">
-                <b-col class="col-lg-3 text-center">
-                    <b-button variant="outline-primary" size="lg" @click="signInWithGoogle">
-                        <b-icon icon="google"/> Log in with Google</b-button>
-                </b-col>
-            </b-row>
-            <b-row class="justify-content-md-center">
-                <b-col class="col-lg-6">
-                    <hr>
-                </b-col>
-            </b-row>
-            <b-row class="justify-content-md-center">
-                <b-col class="col-lg-3 text-center">
-                    <p class="signUpRef">If you have an account: <b-link to="SignIn">Log in</b-link></p>
+                    <b-button type="submit" variant="primary" size="lg">Sign up</b-button>
                 </b-col>
             </b-row>
         </b-form>
+        <b-row class="justify-content-md-center mt-3">
+            <b-col class="col-lg-6">
+                <hr class="or">
+            </b-col>
+        </b-row>
+        <b-row class="justify-content-md-center mt-3 mb-3">
+            <b-col class="col-lg-3 text-center">
+                <b-button variant="outline-primary" size="lg" @click="signInWithGoogle">
+                    <b-icon icon="google"/> Log in with Google</b-button>
+            </b-col>
+        </b-row>
+        <b-row class="justify-content-md-center">
+            <b-col class="col-lg-6">
+                <hr>
+            </b-col>
+        </b-row>
+        <b-row class="justify-content-md-center">
+            <b-col class="col-lg-3 text-center">
+                <p class="signUpRef">If you have an account: <b-link to="SignIn">Log in</b-link></p>
+            </b-col>
+        </b-row>
         <Footer/>
     </b-container>
 </template>
@@ -147,19 +148,63 @@ export default {
     },
     data() {
         return {
+            formError: false,
             userCredentials: {
                 firstName: '',
                 lastName: '',
                 email: '',
                 phone: '',
-                age: '',
-                gender: '',
-                password: ''
+                dateOfBirth: '',
+                gender: 'male',
+                password: '',
+                confirmPassword: ''
             },
+        }
+    },
+    computed: {
+        formState() {
+            return (this.firstNameState == null || this.firstNameState) && 
+                (this.lastNameState == null || this.lastNameState) && 
+                (this.emailState == null || this.emailState) && 
+                (this.phoneState == null || this.phoneState) && 
+                (this.dateOfBirthState == null || this.dateOfBirthState) && 
+                (this.passwordState == null || this.passwordState) &&
+                (this.passwordConfirmState == null || this.passwordConfirmState);
+        },
+        firstNameState() {
+            return this.userCredentials.firstName.length > 2;
+        },
+        lastNameState() {
+            return this.userCredentials.lastName.length > 2;
+        },
+        emailState() {
+            return this.userCredentials.email.indexOf('@') > -1;
+        },
+        phoneState() {
+            return null;
+        },
+        dateOfBirthState() {
+            return this.userCredentials.dateOfBirth != '';
+        },
+        passwordState() {
+            return this.userCredentials.password.length > 3;
+        },
+        passwordConfirmState() {
+            if (this.passwordState) {
+                if (this.userCredentials.password == this.userCredentials.confirmPassword) {
+                    return true;
+                }
+                return false;
+            }
+            return null;
         }
     },
     methods: { 
         signUp() {
+            if (!this.formState) {
+                this.makeToast("Invalid form state", "Please fill out data correctly.", "danger");
+                return;
+            }
             this.$store.dispatch('user/signUp', this.userCredentials).then(() => {
                 this.onSignedIn();
             }).catch((error) => {
@@ -182,8 +227,6 @@ export default {
                 console.log(data);
             }).catch((error) => console.log(error));
         }
-        
-        
     }
 }
 
