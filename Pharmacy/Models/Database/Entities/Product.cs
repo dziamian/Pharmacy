fa712062ab4eb5a9eac8d5750cdaab537833b915
Neutral Effect <56453005+NeutralEffect@users.noreply.github.com>
2021-06-07 22:48:12 +0200

    Added models for remaining db objects

--- Pharmacy/Models/Database/Entities/Product.cs
+++ Pharmacy/Models/Database/Entities/Product.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Pharmacy.Models.Database.Entities
 {
@@ -24,12 +21,19 @@ namespace Pharmacy.Models.Database.Entities
         [Required]
         public long Supply { get; set; }
 
+        [Url]
         [Required]
-        [MaxLength(1023)]
+        [MaxLength(1024)]
         public string Image { get; set; }
 
         [Required]
-        [MaxLength(1023)]
+        [MaxLength(1024)]
         public string Description { get; set; }
-    }
+
+        public ICollection<ProductActiveSubstance> ActiveSubstances { get; set; }
+
+        public ICollection<ProductPassiveSubstance> PassiveSubstances { get; set; }
+
+		public ICollection<Rating> Ratings { get; set; }
+	}
 }
