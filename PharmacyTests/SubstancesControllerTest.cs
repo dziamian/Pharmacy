453b09607d6e59d9c18cd6c184f6f1087f8437b4
Neutral Effect <56453005+NeutralEffect@users.noreply.github.com>
2021-06-15 02:13:45 +0200

    Gitignore update

--- PharmacyTests/SubstancesControllerTest.cs
+++ PharmacyTests/SubstancesControllerTest.cs
@@ -1,13 +1,38 @@
 using System;
 using Xunit;
-using Pharmacy;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace PharmacyTests
 {
-	public class SubstancesControllerTest
+	public class SubstancesControllerTest : IClassFixture<WebApplicationFactory<Pharmacy.Startup>>
 	{
+		private readonly HttpClient m_client;
+
+		public SubstancesControllerTest(WebApplicationFactory<Pharmacy.Startup> fixture)
+		{
+			this.m_client = fixture.CreateClient();
+		}
+
 		[Fact]
-		public void Test1()
+		public async Task CreateActiveSubstanceTest()
+		{
+			var response = await m_client.GetAsync("/api/substances/active");
+			var str = await response.Content.ReadAsStringAsync();
+			throw new Exception();
+			Console.WriteLine(str);
+
+		}
+
+		[Fact]
+		public async Task GetAllActiveSubstancesTest()
+		{
+
+		}
+
+		[Fact]
+		public async Task GetActiveSubstanceByIdTest()
 		{
 
 		}
